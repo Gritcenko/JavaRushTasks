eadebf61cf508ecc605f32cbb6b524f8d77fce96
Tatiana <marmurka.rift@gmail.com>
2021-11-10 22:52:41 +0300

    Task  1801

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task18/task1801/Solution.java
@@ -0,0 +1,32 @@
+package com.javarush.task.pro.task18.task1801;
+
+import java.util.ArrayList;
+
+/* 
+Две реализации одного интерфейса
+В интерфейсе Runnable объявлен метод run() и есть две его реализации: в классе Car и в классе Plane.
+В классе Solution есть публичное статическое поле ArrayList<Runnable> list, а также два публичных статических метода:
+addToList(Runnable), который добавляет в список list элемент, полученный в качестве входного параметра, и метод runList(), который вызывает метод run() у каждого элемента списка list.
+Твоя задача — реализовать эти методы.
+*/
+
+public class Solution {
+    public static ArrayList<Runnable> list = new ArrayList<>();
+
+    public static void main(String[] args) {
+        addToList(new Car());
+        addToList(new Plane());
+
+        runList();
+    }
+
+    public static void addToList(Runnable runnable) {
+        list.add(runnable);//напишите тут ваш код
+    }
+
+    public static void runList() {
+        for (Runnable element: list) {
+            element.run();//напишите тут ваш код
+        }
+    }
+}
